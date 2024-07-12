c431ac48192ab9c5272333059c4de9543f4ac8ac
Maxim Tsoy <mtsoy1990@gmail.com>
2024-07-13 01:08:07 +0900

    test virtual threads

---
+++ src/test/java/com/example/bucket4jtest/TestVirtualThreads.java
@@ -0,0 +1,43 @@
+package com.example.bucket4jtest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.concurrent.CountDownLatch;
+
+import static java.lang.Thread.sleep;
+
+
+@Slf4j
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+
+//-Djdk.tracePinnedThreads=full
+public class TestVirtualThreads {
+
+
+    @Test
+    void testVt() throws InterruptedException {
+        int count = 1;
+        var cdl = new CountDownLatch(count);
+        var obj = new Object(); // something to lock
+
+        for (int i = 0; i < count; i++) {
+            Thread.ofVirtual().name("vt" + i).start(() -> {
+                synchronized (obj) {
+                    log.info(Thread.currentThread().getName() + "(" + Thread.currentThread().threadId() + ")" + ": " + Thread.currentThread().isVirtual());
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                cdl.countDown();
+            });
+        }
+
+        cdl.await();
+    }
+}
